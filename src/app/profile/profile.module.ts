e0d48707279f9c9a5b65f9236d4074f481b8a69b
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-06-20 22:17:12 -0400

    created NavBar components

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileListComponent } from './containers/profile-list/profile-list.component';
 import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [ProfileListComponent, ProfileCardComponent],
+  declarations: [ProfileListComponent, ProfileCardComponent, NavbarComponent],
   imports: [
     CommonModule
   ],
   exports: [
+    NavbarComponent,
     ProfileListComponent,
     ProfileCardComponent
   ]
