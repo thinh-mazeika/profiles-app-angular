3096125b28bfb63a035f1b4fd9af58ba279268ca
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-06-21 22:57:24 -0400

    refactoring function names

--- src/app/profile/services/profile.service.ts
+++ src/app/profile/services/profile.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Person } from '../models/person';
 import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map, filter, tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,29 +11,31 @@ export class ProfileService {
   private profilesUrl = 'public/profiles';
   private profiles: Person[] = [];
   private profilesSubject = new BehaviorSubject<Person[]>([]);
-  private searchSubject = new BehaviorSubject<string>('');
+  private searchTermSubject = new BehaviorSubject<string>('');
 
   constructor(private http: HttpClient) {
+    this.intializeProfiles();
+  }
+
+  getProfiles$(): Observable<Person[]> {
+    return combineLatest(
+      this.profilesSubject.asObservable(),
+      this.searchTermSubject.asObservable()
+    ).pipe(
+      map(([profiles, searchTerm]) => this.filterProfiles(profiles, searchTerm))
+    );
+  }
+
+  updateSearchTerm(searchTerm: string): void {
+    this.searchTermSubject.next(searchTerm);
+  }
+
+  private intializeProfiles(): void {
     this.http
       .get<Person[]>(this.profilesUrl)
       .pipe(
         tap((data) => console.log('profiles', JSON.stringify(data))),
-        map((response: any) =>
-          response.sort(function (a, b) {
-            var nameA = a.name.toLowerCase();
-            var nameB = b.name.toLowerCase();
-
-            if (nameA < nameB) {
-              return -1;
-            }
-
-            if (nameB > nameA) {
-              return 1;
-            }
-
-            return 0;
-          })
-        )
+        map((profiles: Person[]) => profiles.sort(this.sortProfiles))
       )
       .subscribe((profiles) => {
         this.profiles = profiles;
@@ -41,18 +43,22 @@ export class ProfileService {
       });
   }
 
-  getProfiles$(): Observable<Person[]> {
-    return combineLatest([
-      this.profilesSubject.asObservable(),
-      this.searchSubject.asObservable(),
-    ]).pipe(map(([profiles, search]) => this.performSearch(profiles, search)));
+  private sortProfiles(a: Person, b: Person): number {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+
+    if (nameB > nameA) {
+      return 1;
+    }
+
+    return 0;
   }
 
-  updateSearchTerm(searchTerm: string): void {
-    this.searchSubject.next(searchTerm);
-  }
-
-  private performSearch(profiles: Person[], searchTerm: string) {
+  private filterProfiles(profiles: Person[], searchTerm: string) {
     searchTerm = searchTerm.toLocaleLowerCase();
     return profiles.filter(
       (profile: Person) =>
