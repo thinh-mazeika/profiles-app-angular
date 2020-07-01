12628afb0c153666812acd306df2980d56bb0ee6
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-07-01 14:04:51 -0400

    created edit component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { DeleteProfileComponent } from './components/modals/delete/delete-profile/delete-profile.component';
 import { AddProfileComponent } from './components/modals/add/add-profile/add-profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EditComponent } from './components/modals/edit/edit.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     NavbarComponent,
     DeleteProfileComponent,
     AddProfileComponent,
+    EditComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [NavbarComponent, ProfileListComponent, ProfileCardComponent],
