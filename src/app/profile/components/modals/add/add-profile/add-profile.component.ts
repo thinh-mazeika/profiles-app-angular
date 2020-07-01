f661702516d407f6216c3907477e8d788e0fcb73
Thinh Mazeika <thinh.mazeika@gmail.com>
2020-06-30 21:58:50 -0400

    toggle social icons and push the array into the form value

--- src/app/profile/components/modals/add/add-profile/add-profile.component.ts
+++ src/app/profile/components/modals/add/add-profile/add-profile.component.ts
@@ -21,6 +21,18 @@ import { usStates } from './usStates';
 export class AddProfileComponent implements OnInit {
   states = usStates;
 
+  socialProfileMap = {
+    facebook: false,
+    twitter: false,
+    instagram: false,
+    linkedin: false,
+    google: false,
+  };
+
+  get socialProfiles() {
+    return Object.keys(this.socialProfileMap);
+  }
+
   addForm = this.fb.group({
     picUrl: ['', Validators.required],
     name: ['', Validators.required],
@@ -37,8 +49,28 @@ export class AddProfileComponent implements OnInit {
   ) {}
 
   addProfile() {
+    const socialProfilesArray = this.socialProfiles.filter((socialProfile) => {
+      return this.socialProfileMap[socialProfile];
+    });
+    const result = (this.addForm.value.socialProfiles = socialProfilesArray);
+    console.log(result);
     this.profileService.addProfile(this.addForm.value);
     this.addModal.close();
+  }
+
+  getImagePath(socialProfile: string): string {
+    const isOn = this.socialProfileMap[socialProfile];
+    const imagePath = './assets/images/';
+    const iconFileName =
+      socialProfile.charAt(0).toUpperCase() + socialProfile.slice(1);
+    const iconState = isOn ? 'Icon.png' : 'IconOff.png';
+    return imagePath + iconFileName + iconState;
+  }
+
+  toggleIcon(socialProfile: string): void {
+    this.socialProfileMap[socialProfile] = !this.socialProfileMap[
+      socialProfile
+    ];
   }
 
   ngOnInit(): void {}
